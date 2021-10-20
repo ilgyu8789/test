66bc2ddf95f8f21ba4c290f66d23a32050631a3b
박일규 <t5nn123@naver.com>
2021-10-20 17:46:11 +0900

    api 연동 구현

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Users() {
-  //  리액트 훅 -> 함수컴포넌트에서 usestate를 사용하기 위해서
   const [users, setUsers] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -16,8 +15,7 @@ function Users() {
         // loading 상태를 true 로 바꿉니다.
         setLoading(true);
         const response = await axios.get(
-          //  비동기처리방식
-          "http://192.168.0.32:8080/myportal/mysql",
+          "https://jsonplaceholder.typicode.com/users",
         );
         setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
       } catch (e) {
@@ -34,8 +32,10 @@ function Users() {
   if (!users) return null;
   return (
     <ul>
-      {users.citys.map((user) => (
-        <li key={user.cityId}>({user.city})</li>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.username} ({user.name})
+        </li>
       ))}
     </ul>
   );
